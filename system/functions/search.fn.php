86e8465c929dc8188b6cd0e6ac872ea15478518c
Lorze Widmer <lorenz.widmer@gmx.ch>
2012-06-06 22:28:49 +0200

    merged project::getPid() and search function

--- system/functions/search.fn.php
+++ system/functions/search.fn.php
@@ -1,6 +1,48 @@
 <?php
-function search()
+function search($order_by, $order_desc, $filter)
 {
-	
+	if (!is_bool($order_desc))
+		$this->throwError('$order_desc isn`t a bool');
+	if (!is_array($filter))
+		$this->throwError('$filter isn`t an array');
+
+	switch ($order_by)
+	{
+		case ORDER_NAME:
+			$query['order'] = 'name';
+			break;
+		case ORDER_OWNER:
+			$query['order'] = 'owner';
+			break;
+		case ORDER_LIST:
+			$query['order'] = 'list';
+			break;
+		case ORDER_UPTIME:
+			$query['order'] = 'upload_time';
+			break;
+		default:
+			$query['order'] = 'name';
+			break;
+	}
+
+	$query['desc'] = '';
+	$query['filter'] = $filter;
+	$query['user'] = getUser()->id;
+	$query['class'] = getUser()->class;
+	$query['userAL'] = getUser()->access_level;
+
+	if ($order_desc)
+		$query['desc'] = 'DESC';
+
+	if (!$query = getDB()->query('getPid', $query))
+		return false;
+
+	do
+	{
+		$result[] = $query->dataArray[0];
+	}
+	while ($query->next());
+
+	return $result;
 }
 ?>
