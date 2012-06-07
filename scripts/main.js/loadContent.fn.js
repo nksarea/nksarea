0f87031e67ae81a6e7b0393733691919dc05236a
cedl <info@cedl.ch>
2012-06-07 19:54:45 +0200

    contentLoader.js: added ability to load styles and scripts asynchronously

--- scripts/main.js/loadContent.fn.js
+++ scripts/main.js/loadContent.fn.js
@@ -1,17 +1,11 @@
 /*
- * Inhaltsmanagement: Funktionen für asynchrones Laden von Inhalten und in-auftrag-
- * geben von Jobs.
- *
- * @author Cédric Neukom
- */
-
-/*
  * Lädt Inhalt nach.
  *
  * @param e Pfad, HTMLElement oder Event
  * @param post POST-Inhalte, falls nicht gesetzt, ein GET Request wird ausgeführt
  * @param ct Content-Type Header, falls nicht gesetzt, aber POST Request:
  *						application/x-www-form-urlencoded
+ * @author Cédric Neukom
  */
 function loadContent(e, post, ct) {
 	var evt;
@@ -52,6 +46,7 @@ function loadContent(e, post, ct) {
 	// Antwort verarbeiten
 	switch(xhr.status) {
 		case 200: // OK
+			// Update übernehmen
 			var x = xhr.responseXML.rootElement ?
 				xhr.responseXML.rootElement : // IE 8
 				xhr.responseXML.documentElement.childNodes;
@@ -60,18 +55,43 @@ function loadContent(e, post, ct) {
 			for(var i = 0; i < x.length; i++)
 				switch(x[i].tagName) {
 					case 'title':
+						// neuen Dokumenttitel übernehmen
 						document.title = x[i].firstChild.nodeValue;
 						break;
 
 					case 'section':
+						// Feldinhalt aktualisieren
 						if((a = document.querySelector(x[i].getAttribute('target'))))
 							a.initialize(x[i]);
 						break;
 
 					case 'link':
+					case 'script':
+						// Styles und Scripts nachladen, falls nötig
+
+						// Attribute sammeln; querySelector vorbereiten
+						var attr = [];
+						for(var n = 0; n < x[i].attributes.length; n++)
+							attr.push(x[i].attributes[n].name+'="'+x[i].attributes[n].value+'"');
+
+						// Prüfen, ob ein Element mit den Attributen bereits gefunden werden kann
+						if(!document.querySelector(x[i].tagName+'['+attr.join('][')+']')) {
+							var tag = document.createElement(x[i].tagName);
+							for(var n = 0; n < x[i].attributes.length; n++)
+								tag.setAttribute(x[i].attributes[n].name, x[i].attributes[n].value);
+
+							// script/link Tag einhängen; dokument nachladen
+							if(document.head)
+								document.head.appendChild(tag);
+							else
+								document.body.appendChild(tag); // IE 8
+							console.log('added tag '+x[i].tagName);//
+						}
 						break;
 
-					case 'script':
+					case 'report':
+						// Meldung für Benutzer anzeigen
+						// TODO Template::report();
 						break;
 				}
 			break;
