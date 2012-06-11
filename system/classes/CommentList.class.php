29955c8a6c1ad6505c7c735ad43f9d92dce6dc5b
cedl <info@cedl.ch>
2012-06-11 23:31:28 +0200

    added class CommentList

--- system/classes/CommentList.class.php
+++ system/classes/CommentList.class.php
@@ -7,8 +7,103 @@
  */
 class CommentList extends base {
 
-	public function __construct($id) {
+	/** Die ID des Objekts */
+	protected $id;
+
+	/** Der Typ des Objekts (als String; kann in Datenbankabfragen gebraucht werden) */
+	protected $type;
+
+	/** Anzahl Kommentare gesamt */
+	protected $length;
+
+	/** Anzahl Root-Kommentare (Kommentare, die nicht Antwort auf anderen Kommentar sind) */
+	protected $threads;
+
+	/** Erstellt ein neues CommentList-Objekt.
+	 *
+	 * @param integer $objType Eine TYPE-Konstante, die angibt, von welchem Typ
+	 *				das Objekt ist, zu dem Kommentare verarbeitet werden sollen
+	 * @param integer $objID Die ID des Objektes
+	 */
+	public function __construct($objType, $objID) {
+		// Parameter validieren
 		
+		$this->id = (int)$objID;
+		if(!$this->id)
+			$this->throwError('The object id has to be an integer.');
+
+		switch($objType) {
+			case self::TYPE_PROJECT:
+				$this->type = 'project';
+				break;
+
+			case self::TYPE_FILE:
+				$this->type = 'file';
+				break;
+
+			case self::TYPE_LIST:
+				$this->type = 'list';
+				break;
+
+			default:
+				return $this->throwError('The type of the object is invalid. Make sure that it is a TYPE-Constant.');
+		}
+
+		// Anzahl Threads und Kommentare auslesen
+		$result = getDB()->query('getCommentListInfo', array(
+			'type' => $this->type,
+			'id' => $this->id
+		));
+		if($result) {
+			$this->length = $result->dataObj->length;
+			$this->threads = $result->dataObj->threads;
+		}
 	}
 
+	public function __get($key) {
+		switch($key) {
+			case 'length':
+			case 'threads':
+				return $this->$key;
+			case 'comments':
+				return $this->getComments();
+		}
+	}
+
+	/** Liest alle Kommentare aus der Datenbank und gibt sie strukturiert zurück.
+	 *
+	 * Diese Funktion cachet das Ergebnis nicht!
+	 *
+	 * @param boolean $reverseOrder gibt an, ob die Kommentare in umgekehrter
+	 *				Reihenfolge ausgelesen werden sollen. Standard für die Sortierung
+	 *				ist nach Datum aufsteigend (neuestes zu unterst).
+	 */
+	public function getComments($reverseOrder = false) {
+		// Kommentare auslesen
+		$result = getDB()->query('getComments', array(
+			'type' => $this->type,
+			'id' => $this->id,
+			'order' => $reverseOrder?'DESC':'ASC'
+		));
+
+		if(!$result)
+			return $this->throwError('A technical error occurred. No comments could be fetched.');
+
+		else { // Kommentare strukturieren
+			$byID = array();
+			$comments = array();
+
+			do {
+				$comment = $result->dataObj;
+				$comment->replies = array();
+				if((int)$result->dataObj->parent)
+					$byID[$result->dataObj->parent]->replies[] = &$comment;
+				else
+					$comments[] = &$comment;
+				$byID[$result->dataObj->id] = &$comment;
+				unset($comment);
+			} while($result->next());
+		}
+		return $comments;
+	}
 }
---
+++ system/template/sql/getComments.sql
@@ -0,0 +1,12 @@
+-- Liest alle Kommentare zu einem Objekt aus
+
+-- @param type string der Typ des Objekts
+-- @param id integer die ID des Objekts
+-- @param order ASC|DESC die Sortierreihenfolge
+
+SELECT c.`id`, c.`text`, c.`date`, c.`author` AS `authorID`, c.`parent`, a.`name` AS `author`
+FROM `comments` c
+	JOIN `users` a
+		ON c.`author` = a.`id`
+WHERE `%{type}%` = '%{id}%'
+ORDER BY `id` ASC, `date` %{order}%
