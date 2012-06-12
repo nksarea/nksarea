7ad6a617d8765816d8f8084793dc0f2d2ab2df8d
Lorze Widmer <lorenz.widmer@gmx.ch>
2012-06-12 23:48:48 +0200

    added elist page

--- system/contents/pages/list.php
+++ system/contents/pages/list.php
@@ -1,7 +1,36 @@
 <?php
+if ($template instanceof Template)
+{
+	if (!empty($_GET['lid']))
+	{
+		$list = new elist($_GET['lid']);
+		$projectTemplate = new Template(SYS_TEMPLATE_FOLDER.'html/list.xhtml');
+		$template->assign('#content', $projectTemplate);
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+		$template->addJS('http://ajax.googleapis.com/ajax/libs/jquery/1.5/jquery.min.js');
+		$template->addJS('scripts/pageScripts.js');
+		$template->addCSS('styles/css/color-150.css');
+		
+		$projectTemplate->assign('name', $list->data->name);
+		
+		$projectTemplate->assignFromNew('infoTable', 'project/headRow.xhtml', array('key1' => 'Creator:', 'value1' => (string)$list->data->owner, 'key2' => 'Date:', 'value2' => (string)$list->data->creation_time));
+		$projectTemplate->assignFromNew('infoTable', 'project/headRow.xhtml', array('key1' => 'Class:', 'value1' => (string)$list->data->class, 'key2' => 'Type', 'value2' => (string)$list->type));
+		$projectTemplate->assignFromNew('infoTable', 'project/headRow.xhtml', array('key1' => 'Deadline:', 'value1' => (string)$list->data->deadline, 'key2' => '', 'value2' => ''));
+		
+		$projects = getDB()->query('getProjectsList', array('list' => $_GET['lid']));
+		$deadline = new DateTime($list->data->deadline);
+		do
+		{
+			$upload_time = new DateTime($projects->dataObj->upload_time);
+			if($deadline >= $upload_time)
+				$projectTemplate->assignFromNew('projects', 'list/project.xhtml', array('name' => $projects->dataObj->name, 'owner' => $projects->dataObj->owner, 'date' => $projects->dataObj->upload_time, 'color' => $projects->dataObj->color, 'icon' => $projects->dataObj->id . '.jpg'));
+			else
+				$projectTemplate->assignFromNew('projects', 'list/projectFail.xhtml', array('name' => $projects->dataObj->name, 'owner' => $projects->dataObj->owner, 'date' => $projects->dataObj->upload_time, 'icon' => $projects->dataObj->id . '.jpg'));
+		} while ($projects->next());
+	}
+	else
+	{
+		//@TODO: Add error handling;
+	}
+}
 ?>
--- system/contents/pages/project.php
+++ system/contents/pages/project.php
@@ -5,8 +5,47 @@ if ($template instanceof Template)
 {
 	if (!empty($_GET['pid']))
 	{
+		$project = new project($_POST['pid']);
+		if(!empty($_POST['description']) && strlen($_POST['description']) <= 2048)
+		{
+			$_POST['description'] = strip_tags($_POST['description']);
+			$_POST['description'] = str_replace("\n", '{newline}', $_POST['description']);
+			$project->description = $_POST['description'];
+		}
+		if(!empty($_POST['name']) && strlen($_POST['name']) <= 64)
+		{
+			$_POST['name'] = strip_tags($_POST['name']);
+			$project->name = $_POST['name'];
+		}
+		if(!empty($_POST['version']) && preg_match('/[0-9]+\.[0-9]+\.[0-9]+/', $_POST['version']))
+		{
+			if(!empty($_POST['versionfile']) && is_file(SYS_TEMP_FOLDER . $_POST['versionfile']))
+				$project->setVersion($version, $_POST['version']);
+			else
+				$project->setVersion($version, $_POST['versionfile']);
+		}
+		if(!empty($_POST['info']) && is_array($_POST['info']))
+		{
+			$info = $project->info;
+			foreach($_POST['info'] as $key => $value);
+			{
+				if(strlen($key) == 0 || strlen($key) > 15 || strlen($value) > 22)
+					continue;
+				$key = strip_tags($key);
+				$value = strip_tags($value);
+				if(strlen($value) == 0)
+					$value = NULL;
+				$project->setInfo($key, $value);
+				unset($_POST['info'][$key]);
+			}
+			foreach($info as $key => $value);
+				$project->setInfo($key, NULL);
+		}
+		if(!empty($_POST['iconfile']) && is_file(SYS_TEMP_FOLDER . $_POST['iconfile']))
+		{
+			$project->icon = $_POST['iconfile'];
+		}
 
-		$project = new project($_GET['pid']);
 		$projectTemplate = new Template(SYS_TEMPLATE_FOLDER.'html/project.xhtml');
 		$template->assign('#content', $projectTemplate);
 		$template->addJS('http://ajax.googleapis.com/ajax/libs/jquery/1.5/jquery.min.js');
@@ -35,7 +74,7 @@ if ($template instanceof Template)
 		for($i = count($keys)/2; $i <= 3; $i ++)
 			$projectTemplate->assignFromNew('infoTable', 'project/headRow.xhtml', array('key1' => '', 'value1' => '', 'key2' => '', 'value2' => ''));
 		
-		$projectTemplate->assign('description', $project->description);
+		$projectTemplate->assign('description', str_replace('{newline}', '<br />', $project->description));
 		createFilesList($project->viewContent(), $projectTemplate, '/', 'show');
 		
 	}
